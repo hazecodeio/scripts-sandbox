bda4f80f8c87bf4a1768b3ab610aa127d5c87ceb
hsmak <husain.alkhamis@gmail.com>
2022-07-31 12:09:22 -0500

    committing changes...

--- __etc/update-forks.sh
+++ __etc/update-forks.sh
@@ -3,5 +3,5 @@
 cd ..
 ## gnome-reorder-workspaces
 #mkdir -p _forks
-git subtree add --prefix _forks/gnome/gnome-reorder-workspaces/ https://github.com/smmr0/gnome-reorder-workspaces.git master
-git subtree pull --prefix _forks/gnome/gnome-reorder-workspaces/ https://github.com/smmr0/gnome-reorder-workspaces.git master
+git subtree add --prefix _forks/gnome/gnome-reorder-workspaces/ https://github.com/smmr0/gnome-reorder-workspaces.git main
+git subtree pull --prefix _forks/gnome/gnome-reorder-workspaces/ https://github.com/smmr0/gnome-reorder-workspaces.git main
