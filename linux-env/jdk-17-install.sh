f975ca219ffe297ad3bccc006f3942a2189926d2
hsmak <husain.alkhamis@gmail.com>
2022-08-11 09:38:14 -0500

    committing changes...

--- linux-env/jdk-17-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-suffix=_17
-jdk_home=/opt/langs/java/jdk17-current
-jdk_bin=$jdk_home/bin
-dst=/usr/bin
-
-find $jdk_bin -type f -regextype posix-extended -regex '(./|/).*' | grep --color -iE '[a-Z]+$' -o | xargs echo -n | jq --arg suffix $suffix -R 'split(" ") | .[]' | xargs -i update-alternatives --install $dst/{}$suffix {}$suffix $jdk_bin/{} 1
